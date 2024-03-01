8a606ec733d2333072ddbea1f0fc2f568835496f
ajayanga <angasriajaykumar@gmail.com>
2024-03-01 13:52:07 +0530

    pages linked

--- main/views.py
+++ main/views.py
@@ -8,14 +8,14 @@ def student_signup(request):
 
 # Class
 def attendance(request):
-    return render(request, "student/attendance.html")
-def timetable(result):
-    pass
+    return render(request, "student/class/attendance.html")
+def timetable(request):
+    return render(request, "student/class/timetable.html")
 
 # Result
-def ia_result(result):
-    pass
-def model_result(result):
-    pass
-def sem_result(result):
-    pass
+def ia_result(request):
+    return render(request, "student/result/ia.html")
+def model_result(request):
+    return render(request, "student/result/model.html")
+def sem_result(request):
+    return render(request, "student/result/sem.html")
