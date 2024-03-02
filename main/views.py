f010da2c77484e9cf80e07a9ad7a92ee8a9588e3
ajayanga <angasriajaykumar@gmail.com>
2024-03-02 15:16:10 +0530

    Attendance

--- main/views.py
+++ main/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from . import models
+from datetime import datetime
+import calendar
 
 # Login & SignUp
 def user_login(request):
@@ -6,9 +9,41 @@ def user_login(request):
 def student_signup(request):
     pass
 
+def cal_attendance(start_date, attendance, holidays):
+    completed_holidays = []
+    for holiday in holidays:
+        print(holiday.date, holiday.date <= datetime.now().date() and holiday.date >= start_date)
+        if holiday.date <= datetime.now().date() and holiday.date >= start_date:
+            completed_holidays.append(holiday)
+    days = (datetime.now().date() - start_date).days + 1
+    working_days = days - len(completed_holidays)
+    present_days = []
+    for day in attendance:
+        if day.date <= datetime.now().date() and day.date >= start_date:
+            present_days.append(holiday)
+    attendance_percent = int(len(present_days)/working_days*100)
+    return attendance_percent
+
 # Class
 def attendance(request):
-    return render(request, "student/class/attendance.html")
+    month = datetime.now().month
+    year = datetime.now().year
+    user = request.user
+    student = models.Student.objects.get(user=user)
+    user_attendance = models.Attendance.objects.filter(student=student)
+    holidays = models.Holiday.objects.filter(class_related=student.class_attending)
+
+    month_percent = cal_attendance(start_date=datetime(year, month, 1).date(), attendance=user_attendance, holidays=holidays)
+
+    sem_percent = cal_attendance(start_date=student.class_attending.start_date, attendance=user_attendance, holidays=holidays)
+
+    data = {
+        "sem_percent" : sem_percent,
+        "month_percent" : month_percent,
+        "student" : student,
+    }
+    return render(request, "student/class/attendance.html", data)
+
 def timetable(request):
     return render(request, "student/class/timetable.html")
 
